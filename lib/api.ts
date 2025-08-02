4e75b3145f93f52699a0793206ea82393db2cece
cavassani <lucas.cavassani.q@gmail.com>
2025-08-01 22:43:43 -0300

    adição do diretório das bibliotecas para o projeto

---
+++ lib/api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+import MY_TOKEN_KEY from "./get-cookie-name";
+
+export const api = axios.create({
+    baseURL: `/api`,
+
+    headers: {
+        cache: "no-store"
+    }
+});
+
+export const apiServer = axios.create({
+    baseURL: process.env.NEXT_APP_API_URL as string,
+
+    headers: {
+        cache: "no-store"
+    }
+});
+
+api.interceptors.request.use(
+    async (config) => {
+        // obtém o token dos cookies
+        const cookie_name = MY_TOKEN_KEY();
+
+        const token = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith(`${cookie_name}=`))
+            ?.split("=")[1];
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    }, (error) => {
+        // auxilia o erro
+        return Promise.reject(error);
+    }
+);
